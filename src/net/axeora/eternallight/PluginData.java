2823e72c0783704d52567a55921356434e5771cc
Matthew Denton <matthew.denton77@gmail.com>
2017-12-21 12:56:00 +1300

    Updated resource ID

--- src/net/axeora/eternallight/PluginData.java
+++ src/net/axeora/eternallight/PluginData.java
@@ -4,7 +4,7 @@ public class PluginData {
 
     public static final String NAME = "EternalLight";
     public static final String VERSION = "1.0";
-    public static final int RESOURCE_ID = 25618;
+    public static final int RESOURCE_ID = 50961;
 
     public static final String PREFIX = "&e&l[" + NAME + "]&f ";
 }
